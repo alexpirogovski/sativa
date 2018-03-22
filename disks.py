10f5ef69b50c8a32aa26ceedcf7c65bd1aa13e1a
alexp <pirogovski@gmail.com>
2018-03-22 15:52:03 +0200

    Bulk commit

--- disks.py
+++ disks.py
@@ -1,5 +1,4 @@
-import itertools
-
+from data_holder import *
 
 class Disk(object):
     def __init__(self, name):
@@ -13,6 +12,12 @@ class Disk(object):
         print self._disk_name
         print self._data
 
+    def get_data_by_name(self, header):
+        if header in self._data.keys():
+            return self._data[header]
+        else:
+            return None
+
 
 class IOReportsList(object):
     def __init__(self):
@@ -24,6 +29,14 @@ class IOReportsList(object):
     @property
     def report_list(self):
         return self._report_list
+
+    def get_disk_report_by_name(self, disk_name, name):
+        pass
+        # g = MyGraph(disk_name)
+        # for report in self._report_list:
+        #     for disk in report.get_disks:
+        #         p = Point(report.timestamp, disk.get_data_by_name(name))
+        # return g
 
 
 class IOReport(object):
@@ -38,8 +51,7 @@ class IOReport(object):
 
     def add_disk(self, line):
         self._disks.append(Disk(line[0]))
-        del line[0]
-        self._disks[-1].populate_data(self._headers, line)
+        self._disks[-1].populate_data(self._headers, line[1:])
 
     def set_headers(self, l):
         self._headers = dict.fromkeys(l, None)
